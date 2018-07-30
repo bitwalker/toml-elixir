5017e600025bd43ae73068afa3485cc038736c16
Paul Schoenfelder <paulschoenfelder@gmail.com>
2018-07-29 23:28:51 -0400

    Version 0.2.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Toml.MixProject do
   def project do
     [
       app: :toml,
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       consolidate_protocols: Mix.env() != :test,
