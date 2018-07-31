eb387170c62c15889c2a8ef4fa91c61f6de9b23e
Paul Schoenfelder <paulschoenfelder@gmail.com>
2018-07-31 11:34:22 -0400

    Fix expected json for some tests, add many more tests, clean up existing test organization

---
+++ test/decoder_test.exs
@@ -0,0 +1,62 @@
+defmodule Toml.Test.DecoderTests do
+  @moduledoc """
+  Like `Toml.Test.TomlTestTests`, but runs against a set of collected valid and invalid tests
+  which aren't part of `toml-test`
+  """
+  use ExUnit.Case
+  
+  import Toml.Test.Assertions
+  
+  @test_dir Path.join([__DIR__, "fixtures"])
+  @valid_tests Path.wildcard(Path.join([@test_dir, "valid", "*.toml"]))
+  @num_valid length(@valid_tests)
+  @invalid_tests Path.wildcard(Path.join([@test_dir, "invalid", "*.toml"]))
+  
+  
+  # For each test, we want to generate useful line information for `mix test path/to/test:line` filters
+  # So we map an index to each test
+  for {valid_test, line} <- Enum.with_index(@valid_tests), name = Path.basename(valid_test, ".toml") do
+    # Then offset it using the current line in the environment
+    line = __ENV__.line + line
+    # Build the test definition
+    quoted =
+      quote line: line do
+        @toml_test unquote(name)
+        @toml_test_path unquote(valid_test)
+        @toml_test_type :valid
+        @tag toml_test: @toml_test
+        @tag toml_test_type: :valid
+        test "#{@toml_test} is valid" do
+          assert_toml_valid(@toml_test_path)
+        rescue
+          err in [ExUnit.AssertionError] ->
+            # Dump the TOML for better test failure context
+            msg = err.message <> "\nExpected the following TOML to be considered valid:\n\n---\n#{File.read!(@toml_test_path)}\n---"
+            reraise %ExUnit.AssertionError{err | message: msg}, System.stacktrace()
+        end
+      end
+    # And eval it in the module environment, resulting in a test as if it was defined by hand
+    # rather than in the body of a loop
+    Module.eval_quoted(%Macro.Env{__ENV__ | line: line}, quoted)
+  end
+  
+  # The only difference for the invalid tests is that we additionally offset it by the number of valid tests
+  for {invalid_test, line} <- Enum.with_index(@invalid_tests), name = Path.basename(invalid_test, ".toml") do
+    line = __ENV__.line + line + @num_valid
+    quoted =
+      quote line: (@num_valid + __ENV__.line + line) do
+        @toml_test unquote(name)
+        @toml_test_path unquote(invalid_test)
+        @tag toml_test: @toml_test
+        @tag toml_test_type: :invalid
+        test "#{@toml_test} is invalid" do
+          assert {:error, {:invalid_toml, _}} = Toml.decode_file(@toml_test_path)
+        rescue
+          err in [ExUnit.AssertionError] ->
+            msg = err.message <> "\n\nExpected the following TOML to be considered invalid:\n\n---\n#{File.read!(@toml_test_path)}\n---"
+            reraise %ExUnit.AssertionError{err | message: msg}, System.stacktrace()
+        end
+      end
+    Module.eval_quoted(%Macro.Env{__ENV__ | line: line}, quoted)
+  end
+end
--- test/toml_test.exs
+++ test/toml_test.exs
@@ -3,52 +3,7 @@ defmodule Toml.Test do
   
   import Toml.Test.Assertions
 
-  describe "basic syntax" do
-    test "integers" do
-      assert {:ok, %{"n" => 1}} = decode("n = 1")
-      assert {:ok, %{"n" => 1}} = decode("n = +1")
-      assert {:ok, %{"n" => -1}} = decode("n = -1")
-      assert {:ok, %{"n" => 105}} = decode("n = 105")
-      assert {:ok, %{"n" => 3500}} = decode("n = 3_500")
-      assert {:ok, %{"n" => -3500}} = decode("n = -3_500")
-    end
-    
-    test "floats" do
-      assert {:ok, %{"n" => 1.0}} = decode("n = 1.0")
-      assert {:ok, %{"n" => 1.0}} = decode("n = +1.0")
-      assert {:ok, %{"n" => -1.0}} = decode("n = -1.0")
-      assert {:ok, %{"n" => 1.0e2}} = decode("n = 1.0e2")
-      assert {:ok, %{"n" => 1.0e2}} = decode("n = 1e2")
-      assert {:ok, %{"n" => 1.0e2}} = decode("n = 1.0E2")
-      assert {:ok, %{"n" => 1.0e2}} = decode("n = 1E2")
-    end
-    
-    test "hexadecimal" do
-      assert {:ok, %{"n" => 0xAE}} = decode("n = 0xAE")
-      assert {:ok, %{"n" => 0xAE}} = decode("n = 0xae")
-    end
-    
-    test "octal" do
-      assert {:ok, %{"n" => 0o777}} = decode("n = 0o777")
-    end
-    
-    test "binary numbers" do
-      assert {:ok, %{"n" => 0b10101}} = decode("n = 0b10101")
-    end
-    
-    test "basic string" do
-      assert {:ok, %{"n" => "hello world!"}} = decode("n = \"hello world!\"")
-    end
-    
-    test "literal string" do
-      assert {:ok, %{"n" => "\"hello world!\""}} = decode("n = '\"hello world!\"'")
-    end
-
-    test "quoted string" do
-      assert {:ok, %{"n" => "hello world!"}} = decode("n = \"hello world!\"")
-      assert {:ok, %{"n" => "\"hello world!\""}} = decode("n = \"\\\"hello world!\\\"\"")
-    end
-    
+  describe "date/time types" do
     test "date" do
       assert {:ok, %{"n" => ~D[2018-06-30]}} = decode("n = 2018-06-30")
       assert {:error, {:invalid_toml, _}} = decode("n = 2018-16-30")
@@ -79,39 +34,6 @@ defmodule Toml.Test do
       expected = DateTime.from_naive!(~N[2018-06-30T05:30:58.030], "Etc/UTC")
       assert {:ok, %{"n" => ^expected}} = decode("n = 2018-06-30 12:30:58.030-07:00")
     end
-    
-    test "bare keys" do
-      assert {:ok, %{"n" => 1}} = decode("n = 1")
-      assert {:ok, %{"n1" => 1}} = decode("n1 = 1")
-      assert {:ok, %{"n_1" => 1}} = decode("n_1 = 1")
-      assert {:ok, %{"n-1" => 1}} = decode("n-1 = 1")
-      assert {:error, {:invalid_toml, _}} = decode("n! = 1")
-    end
-    
-    test "quoted keys" do
-      assert {:ok, %{"key with space" => 1}} = decode("\"key with space\" = 1")
-      assert {:ok, %{"key with $p@c3" => 1}} = decode("\"key with $p@c3\" = 1")
-      assert {:ok, %{"literal key" => 1}} = decode("'literal key' = 1")
-    end
-    
-    test "dotted keys" do
-      assert {:ok, %{"a" => %{"b" => 1}}} = decode("a.b = 1")
-      assert {:ok, %{"a" => %{"b" => %{"c" => 1}}}} = decode("a.b.c = 1")
-      assert {:ok, %{"a" => %{"mixed keys" => 1}}} = decode("a.\"mixed keys\" = 1")
-    end
-    
-    test "tables" do
-      expected = %{}
-      assert {:ok, %{"a" => ^expected}} = decode("[a]\n")
-      assert {:ok, %{"a" => %{"b" => ^expected}}} = decode("[a.b]\n")
-      assert {:ok, %{"a" => %{"foo" => ^expected}}} = decode("[a.\"foo\"]\n")
-      expected = %{"n" => 1}
-      assert {:ok, %{"a" => ^expected}} = decode("[a]\nn = 1")
-      assert {:ok, %{"a" => %{"b" => ^expected}}} = decode("[a.b]\nn = 1")
-      assert {:ok, %{"a" => %{"foo" => ^expected}}} = decode("[a.\"foo\"]\nn = 1")
-      assert {:ok, %{"a" => %{"n" => 1, "b" => %{"n" => 2}}}} = decode("[a]\nn = 1\n[a.b]\nn = 2")
-      assert {:ok, %{"a" => %{"n" => 2, "b" => %{"n" => 1}}}} = decode("[a.b]\nn = 1\n[a]\nn = 2")
-    end
   end
   
   test "example.toml" do
@@ -124,37 +46,12 @@ defmodule Toml.Test do
     assert {:ok, %{table: %{subtable: %{key: "another value"}}}} = Toml.decode_file(input, keys: :atoms)
   end
   
-  test "test.toml" do
-    input = Path.join([__DIR__, "fixtures", "test.toml"])
+  test "0.5.0.toml" do
+    input = Path.join([__DIR__, "fixtures", "0.5.0.toml"])
     assert_toml_valid(input)
   end
 
-  test "test2.toml" do
-    input = Path.join([__DIR__, "fixtures", "test2.toml"])
-    assert_toml_valid(input)
-  end
-  
-  test "hard.toml" do
-    input = Path.join([__DIR__, "fixtures", "hard.toml"])
-    assert_toml_valid(input)
-  end
-  
-  test "hard-unicode.toml" do
-    input = Path.join([__DIR__, "fixtures", "hard-unicode.toml"])
-    assert_toml_valid(input)
-  end
-  
-  test "hard.toml (invalid variants)" do
-    for input <- Path.wildcard(Path.join([__DIR__, "fixtures", "hard-invalid*.toml"])) do
-      assert {:error, {:invalid_toml, _}} = decode_file(input)
-    end
-  end
-  
   defp decode(str) when is_binary(str) do
     Toml.decode(str)
-  end
-  
-  defp decode_file(path) when is_binary(path) do
-    Toml.decode_file(path)
   end
 end
